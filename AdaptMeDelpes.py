3cc07342667b0ea98c168241c5a2309ae29ccffd
Markus Stoye <mstoye@mstoyemac.dyndns.cern.ch>
2018-04-23 15:35:30 +0200

    tidies up, also removed return of output shapes, but get output shapes from model. Less hardcoded and leaner code. Worked interactively.

--- AdaptMeDelpes.py
+++ AdaptMeDelpes.py
@@ -1,7 +1,6 @@
 from argparse import ArgumentParser
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import keras
 from keras import backend as K, callbacks
 import sys
@@ -9,6 +8,9 @@ import tensorflow as tf
 import os
 from pdb import set_trace
 from sklearn.model_selection import train_test_split
+from Layers import GradientReversal
+from keras.models import Model
+from keras.layers import Input
 
 parser = ArgumentParser()
 parser.add_argument('outputDir')
@@ -19,7 +21,10 @@ parser.add_argument(
 		'stepwise_domain_adaptation'
 	]
 )
-parser.add_argument("-i",  help="input directory", default='/data/ml/jkiesele/pheno_domAda/numpyx2/', dest='indir')
+
+parser.add_argument(
+    "-i",  help="input directory",
+    default='/data/ml/jkiesele/pheno_domAda/numpyx2/', dest='indir')
 parser.add_argument("--addsv", action='store_true')
 parser.add_argument("--gpu",  help="select specific GPU",   type=int, metavar="OPT", default=-1)
 parser.add_argument("--nopred", help="do not compute and store predictions", action='store_true')
@@ -65,63 +70,60 @@ def save(df, fname):
 	records = df.to_records(index=False)
 	records.dtype.names = [str(i) for i in records.dtype.names]
 	np.save(fname, records)
-	
 
 
 def schedule(x):
-	lr=args.lr
-	if x>100: lr=args.lr*0.8
-	if x>200: lr=args.lr*0.5
-	if x>400: lr=args.lr*0.1
-	if x>600: lr=args.lr*0.05
-	print('epoch', x, 'learning rate ',lr)
-	return lr
+    lr=args.lr
+    if x>100: lr=args.lr*0.8
+    if x>200: lr=args.lr*0.5
+    if x>400: lr=args.lr*0.1
+    if x>600: lr=args.lr*0.05
+    print('epoch', x, 'learning rate ', lr)
+    return lr
+
 
 learning_rate = keras.callbacks.LearningRateScheduler(schedule)
+nepochstotal = 10
+batchsize = 10000
+global_loss_list = {}
 
-
-nepochstotal=200
-batchsize=10000
-global_loss_list={}
-from Layers import *
-from keras.models import Model
-from keras.layers import Input
 
 def run_model(outdir, training_method=1):
-	
-	Inputs = Input((10,))
-		
-	global_loss_list['GradientReversal']=GradientReversal()
-	X_traintest, isB_traintest , isMC_traintest = make_sample(args.indir, args.addsv)
-	X_all, X_test, isB_all, isB_test, isMC_all, isMC_test = train_test_split(X_traintest, isB_traintest , isMC_traintest, test_size=0.1, random_state=42)
-
-		
-	onesarray=np.ones(isB_all.shape[0])
-	optimizer=Adam(lr=args.lr)
-	from block_models import common_features, btag_discriminator,datamc_discriminator
-	
-	
-	#for compatibility
-	def modelIverseGrad(Inputs,rev_grad):
-		#build the models
-		feat, outshape=common_features(Inputs)
-		btag,_=btag_discriminator(outshape,dropoutrate=0.03)
-		datamc,_=datamc_discriminator(outshape,rev_grad=rev_grad,dropoutrate=0.03)
-		
-		return Model(inputs=Inputs, outputs=btag(feat(Inputs))+datamc(feat(Inputs)),name='fullModel')
-	
-	model = modelIverseGrad(Inputs,rev_grad=args.lmb)
+    Inputs = Input((10,))
+    global_loss_list['GradientReversal'] = GradientReversal()
+    X_traintest, isB_traintest, isMC_traintest = make_sample(
+    args.indir, args.addsv)
+    X_all, X_test, isB_all, isB_test, isMC_all, isMC_test = train_test_split(
+    X_traintest, isB_traintest, isMC_traintest,
+    test_size = 0.1, random_state = 42)
+    onesarray=np.ones(isB_all.shape[0])
+    optimizer = Adam(lr = args.lr)
+    from block_models import common_features, btag_discriminator, datamc_discriminator
 	
 	
+    # for compatibility
+    def modelIverseGrad(Inputs, rev_grad):
+        feat = common_features(Inputs)
+        outshape = Input(feat.get_layer(index=-1).output_shape)
+        btag = btag_discriminator(outshape, dropoutrate=0.03)
+        datamc = datamc_discriminator(
+            outshape, rev_grad=rev_grad, dropoutrate=0.03)
+        return Model(
+            inputs=Inputs, outputs=btag(feat(Inputs)) + datamc(feat(Inputs)),
+            name='fullModel')
 	
-	if(training_method == 'MC_training'):
-		model.compile(
+    model = modelIverseGrad(Inputs, rev_grad=args.lmb)
+	
+	
+	
+    if(training_method == 'MC_training'):
+        model.compile(
 			loss = ['binary_crossentropy']*4,
 			optimizer=optimizer, 
 			loss_weights=[1.,0.,0.,0.],
 			weighted_metrics=['accuracy']
 		)
-		history = model.fit(
+        history = model.fit(
 			X_all,
 			[isB_all, isB_all, isMC_all, isMC_all], 
 			batch_size=batchsize, epochs=nepochstotal, verbose=2, validation_split=0.2,
@@ -132,7 +134,7 @@ def run_model(outdir, training_method=1):
 				1-0.5*isB_all.ravel()],
 			
 		)
-	elif(training_method == 'data_training'):
+    elif(training_method == 'data_training'):
 		model.compile(
 			loss=['binary_crossentropy']*4,
 			optimizer=optimizer, 
@@ -150,57 +152,47 @@ def run_model(outdir, training_method=1):
 				1-0.5*isB_all.ravel()],
 			
 		)
-	elif(training_method == 'stepwise_domain_adaptation'):
-		
-		
-		da_epochs      =nepochstotal
-		stepda  =2
-		stepbtag=2
-		
-		#build the models
-		feat, outshape=common_features(Inputs)
-		btag,_=btag_discriminator(outshape,dropoutrate=0.03)
-		datamc,_=datamc_discriminator(outshape,rev_grad=args.lmb,dropoutrate=0.03)
-		
-		modelallopen = Model(inputs=Inputs, outputs=btag(feat(Inputs))+datamc(feat(Inputs)),name='fullModel')
-
-		#print(feat.summary())
-		#print(btag.summary())
-		#print(datamc.summary())
+    elif(training_method == 'stepwise_domain_adaptation'):
+        da_epochs = nepochstotal
+        stepda = 2
+        stepbtag = 2
+        #build the models
+        feat = common_features(Inputs)
+        outshape = Input(feat.get_layer(index=-1).output_shape)         
+        btag = btag_discriminator(outshape,dropoutrate=0.03)
+        datamc = datamc_discriminator(outshape,rev_grad=args.lmb,dropoutrate=0.03)
+        modelallopen = Model(inputs=Inputs, outputs=btag(feat(Inputs))+datamc(feat(Inputs)),name='fullModel')
 	
-		modelallopen.compile(
+        modelallopen.compile(
 			loss = ['binary_crossentropy']*4,
 			optimizer = optimizer, 
 			loss_weights = [1.,0.,args.weight,0.],
 			weighted_metrics=['accuracy']
 		)
 		
-		def fixLayers(model):
+        def fixLayers(model):
 			for layer in model.layers:
 				layer.trainable = False
 			return model
 		
-		feat=fixLayers(feat)
-		btag=fixLayers(btag)
-		
-		modelfixedbtag = Model(inputs=Inputs, outputs=btag(feat(Inputs))+datamc(feat(Inputs)),name='fullModel2')
-		
-		modelfixedbtag.compile(
+        feat=fixLayers(feat)
+        btag=fixLayers(btag)
+        modelfixedbtag = Model(inputs=Inputs, outputs=btag(feat(Inputs))+datamc(feat(Inputs)),name='fullModel2')
+        modelfixedbtag.compile(
 			loss = ['binary_crossentropy']*4,
 			optimizer = optimizer, 
 			loss_weights = [1.,0.,args.weight,0.],
 			weighted_metrics=['accuracy']
 		)
 		
-		## no user intervention needed here
-		
-		switch=0
-		currentepoch=0
-		step=0
+        ## no user intervention needed here
+        switch=0
+        currentepoch=0
+        step=0
 
-		history=pd.DataFrame()
-		history2=pd.DataFrame()
-		while training_method == 'stepwise_domain_adaptation':
+        history=pd.DataFrame()
+        history2=pd.DataFrame()
+        while training_method == 'stepwise_domain_adaptation':
 			
 			
 			if switch % 2 == 0:
@@ -253,26 +245,26 @@ def run_model(outdir, training_method=1):
 			history=pd.concat([history,history2], ignore_index=True)
 			
 		#doesn't matter which one, but important for the predict part
-		model=modelallopen
+        model=modelallopen
 		
-	else:
+    else:
 		raise ValueError('%s is an unknown run option' % training_method)
 
-	if hasattr(history, 'history'):
+    if hasattr(history, 'history'):
 		history = pd.DataFrame(history.history)
-	save(history, '%s/history.npy' %outdir)
-	if args.nopred:
+    save(history, '%s/history.npy' %outdir)
+    if args.nopred:
 		return history
 
-	predictions = model.predict(X_test)
-	preds = pd.DataFrame()
-	preds['prediction'] = predictions[0].ravel()
-	preds['isB'] = isB_test
-	preds['isMC'] = isMC_test
-	save(preds, '%s/predictions.npy' %outdir)
+    predictions = model.predict(X_test)
+    preds = pd.DataFrame()
+    preds['prediction'] = predictions[0].ravel()
+    preds['isB'] = isB_test
+    preds['isMC'] = isMC_test
+    save(preds, '%s/predictions.npy' %outdir)
 	
 	
-	return history
+    return history
 
 run_model(
 	args.outputDir, 
--- block_models.py
+++ block_models.py
@@ -1,7 +1,4 @@
-
-import keras
-from keras.layers import Dense, Concatenate ,Dropout
-from keras.layers import Input
+from keras.layers import Dense, Dropout
 from keras.models import Model
 from Layers import GradientReversal
 
@@ -12,28 +9,21 @@ from Layers import GradientReversal
 # block consistently
 #
 
+
 def common_features(Inputs, dropoutrate=0.03):
-    X=Inputs
+    X = Inputs
     X = Dense(30, activation='relu', name='common_dense_0')(X)
     X = Dropout(dropoutrate)(X)
-
     X = Dense(20, activation='relu')(X)
     X = Dropout(dropoutrate)(X)
-
     X = Dense(20, activation='relu')(X)
     X = Dropout(dropoutrate)(X)
-    
     X = Dense(15, activation='relu')(X)
     X = Dropout(dropoutrate)(X)
-    
-    #please always define and return this (shape of the last layer)
-    outputDescription=Input((15,))
-    
-    return Model(inputs=Inputs, outputs=X, name = 'common_features'), outputDescription
+    return Model(inputs=Inputs, outputs=X, name='common_features')
 
 
 def datamc_discriminator(XInputs, rev_grad=10, dropoutrate=0.03):
-    
     Ad = GradientReversal(hp_lambda=rev_grad, name='Ad_gradrev')(XInputs)
     Ad = Dense(20, activation='relu')(Ad)
     Ad = Dropout(dropoutrate)(Ad)
@@ -43,31 +33,24 @@ def datamc_discriminator(XInputs, rev_grad=10, dropoutrate=0.03):
     Ad = Dropout(dropoutrate)(Ad)
     Ad = Dense(10, activation='relu')(Ad)
     Ad = Dropout(dropoutrate)(Ad)
-    Ad = Dense(1, activation='sigmoid', name = 'Add' )(Ad)
-    
-    #just for compatibility for now
-    Ad1 = Dense(1, activation='linear',use_bias=False,trainable=False  , name = 'Add1' )(Ad)
-    
-    outputDescription=[Input((1,)),Input((1,))]
-    
-    return Model(inputs=XInputs, outputs=[Ad,Ad1], name="datamc_discriminator"),outputDescription
+    Ad = Dense(1, activation='sigmoid', name='Add')(Ad)
+    # just for compatibility for now
+    Ad1 = Dense(
+        1, activation='linear', use_bias=False, trainable=False,
+        name='Add1')(Ad)
+    return Model(
+        inputs=XInputs, outputs=[Ad, Ad1], name="datamc_discriminator")
+
 
 def btag_discriminator(XInputs, dropoutrate=0.03):
     X = XInputs
     X = Dense(15, activation='relu')(X)
     X = Dropout(dropoutrate)(X)
-    
     X = Dense(15, activation='relu')(X)
     X = Dropout(dropoutrate)(X)
-    
-    X = Dense(1, activation='sigmoid', name = 'mc')(X)
-    
-    X1= Dense(1, activation='linear',use_bias=False, trainable=False,
-            kernel_initializer='Ones', name = 'data') (X)
-    
-    outputDescription=[Input((1,)),Input((1,))]
-            
-    return Model(inputs=XInputs, outputs=[X,X1], name="btag_discriminator"),outputDescription
-
-
-
+    X = Dense(1, activation='sigmoid', name='mc')(X)
+    X1 = Dense(
+        1, activation='linear', use_bias=False, trainable=False,
+        kernel_initializer='Ones', name='data')(X)
+    return Model(
+        inputs=XInputs, outputs=[X, X1], name="btag_discriminator")
