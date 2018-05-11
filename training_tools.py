e5e2452ad5fabd83a72b61c5708e72fd85194c7b
jkiesele <jkiesele@cern.ch>
2018-05-11 12:53:25 +0200

    enable custom label functions

--- training_tools.py
+++ training_tools.py
@@ -14,6 +14,7 @@ def train_models_adversarial(modellist,
                              validation_split,
                              nstepspermodel=None,
                              custombatchgenerators=None,
+                             modify_labels=None,
                              verbose=0):
     '''
     modellist,x,y,sample_weight need to be lists, even if only one entry
@@ -28,6 +29,10 @@ def train_models_adversarial(modellist,
     custombatchgenerators = [None, myfunct]
     then arr2 will be replaced with the output of myfunct(batchsize) for each batch.
     The function given to the list must have the batch size as first argument.
+    
+    modify_labels: applies user defined function to the label list. Needs to be a list
+    with same length as the model list. This function will be called for each batch, so keep it simple
+    
     '''
 
     # create the batches
@@ -39,33 +44,49 @@ def train_models_adversarial(modellist,
     for i in range(ntrainbatches):
         split.append((i+1)*batch_size)
 
+    # this function makes sure the training sample
+    # comprises exactly ntrainbatches batches
     def split_list(in_list):
-        out = [[None for i in range(len(in_list))] for i in range(ntrainbatches)]
+        if not hasattr(in_list, '__getitem__'):
+            return None, None
+        out = [None for i in range(len(in_list))]
         val = [None for i in range(len(in_list))]
         for i in range(len(in_list)):
             sp = np.split(in_list[i], split)
             for j in range(len(sp)-1):
-                out[j][i] = sp[j]
+                if j:
+                    out[i] = np.concatenate((out[i],sp[j]))
+                else:
+                    out[i] = sp[j]
             val[i] = sp[-1]
         return out, val
+    
+    def split_to_batches(in_list):
+        if not hasattr(in_list, '__getitem__'):
+            return [None for i in range(ntrainbatches)]
+        out = [[None for i in range(len(in_list))] for i in range(ntrainbatches)]
+        for i in range(len(in_list)):
+            sp = np.split(in_list[i], split)
+            for j in range(len(sp)-1):
+                out[j][i] = sp[j]
+        return out
 
-    x_split, x_val = split_list(x)
-    y_split, y_val = split_list(y)
-    sw_split = None
-    sw_splitval = None
-    if sample_weight:
-        sw_split, sw_splitval = split_list(sample_weight)
+    x_train, x_val = split_list(x)
+    y_train, y_val = split_list(y)
+    sw_train, sw_splitval = split_list(sample_weight)
 
     # input is prepared
+    # re-write to shuffle the whole list and split again
     def randomise_array_lists():
         rstate = randint(0, 1e4)
-        for a in x_split:
-            a = shuffle(a, random_state=rstate)
-        for a in y_split:
-            a = shuffle(a, random_state=rstate)
-        if sw_split:
-            for a in sw_split:
-                a = shuffle(a, random_state=rstate)
+        for i in range(len(x_train)):
+            x_train[i] = shuffle(x_train[i], random_state=rstate)
+        for i in range(len(y_train)):
+            y_train[i] = shuffle(y_train[i], random_state=rstate)
+        if sw_train:
+            for i in range(len(sw_train)):
+                sw_train[i] = shuffle(sw_train[i], random_state=rstate)
+            
 
     # do this global for epochs
     modelit = 0
@@ -83,7 +104,12 @@ def train_models_adversarial(modellist,
             usedmetrics.append(name)
 
     for e in range(epochs):
+        
         randomise_array_lists()
+        x_split = split_to_batches(x_train)
+        y_split = split_to_batches(y_train)
+        sw_split = split_to_batches(sw_train)
+        
         epochtrainhist = []
         currentbatch = 0
         print('starting adversarial epoch ' + str(e+1) + '/' + str(epochs))
@@ -104,13 +130,20 @@ def train_models_adversarial(modellist,
                                 print('launched generator function '+str(i)+' for batch '+str(currentbatch))
 
                 nsteps = nsteps-1
+                if modify_labels:
+                    if modify_labels[modelit]:
+                        y_split[currentbatch]=modify_labels[modelit](y_split[currentbatch])
+                            
+                
 
+                if verbose > 2:
+                    print('training model ', modelit, ' on batch ', currentbatch)
                 # take last batch for training history
                 epochtrainhist = model.train_on_batch(x_split[currentbatch],
                                                       y_split[currentbatch],
                                                       sample_weight = sw_split[currentbatch])
                 if verbose > 2:
-                    print('training model ', modelit, ' on batch ', currentbatch)
+                    print(epochtrainhist)
 
                 # adjust for next batch
                 if nsteps:
