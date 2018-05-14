aceaf3530fca6c82af6097016319487f3d095721
mauro verzetti <mverzett@cern.ch>
2018-05-14 17:10:36 +0200

    normalize label fractions in the weights and allow skewing

--- make_samples.py
+++ make_samples.py
@@ -1,9 +1,8 @@
 import numpy as np
 from glob import glob
+from pdb import set_trace
 
-
-def make_sample(input_dir, add_svs):
-
+def make_sample(input_dir, add_svs, keep_probability=None): #default value tuned for current sample
     x_file = glob('%s/*features_0.npy' % input_dir)[0]
     y_file = glob('%s/*truth_1.npy' % input_dir)[0]
     X_all = np.load(x_file)
@@ -21,7 +20,7 @@ def make_sample(input_dir, add_svs):
     print(isMC_all.shape)
     # select MC only
     # create random isMC vector
-    # np.random.seed(1234)
+    np.random.seed(1234567)
     # isMC_all = np.random.randint(2, size=X_all.shape[0])
     # isMC_all = np.reshape(isMC_all,(isMC_all.shape[0],1))
     # print(isMC_all.shape)
@@ -50,5 +49,71 @@ def make_sample(input_dir, add_svs):
     # X_2Ds =  X_2Ds + noise * X_2Ds #* X_2Ds * (isMC_all<.1)
     # X_ptRel= noise #* X_3Ds * (isMC_all<.1)
     # X_ptPro= noise #* X_3Ds * (isMC_all<.1)
-    return np.concatenate(
-        [X_ptRel, X_2Ds, X_3Ds, X_ptPro], axis=1), isB_all, isMC_all
+    
+    X_all = np.concatenate([X_ptRel, X_2Ds, X_3Ds, X_ptPro], axis=1)
+    #
+    # flavour weights stuff
+    #
+    ismc = (isMC_all.ravel() > 0.5)
+    isb = (isB_all.ravel() > 0.5)
+    #compute scaling to get same composition
+    cnst = float((isb & np.invert(ismc)).sum())/np.invert(ismc).sum()
+    nb = (isb & ismc).sum()
+    nl = (np.invert(isb) & ismc).sum()
+    fraction = (nb/cnst - nb)/nl
+    if fraction > 1.:
+        fraction = -1*nl/(nb/cnst - nb)
+    print ' -- DATASET --'
+    print 'Before masking:'
+    print 'MC fraction: %.3f' % (float(ismc.sum())/ismc.shape[0])
+    print 'Data B fraction: %.3f' % (float((isb & np.invert(ismc)).sum())/np.invert(ismc).sum())
+    print 'MC B fraction: %.3f' % (float((isb & ismc).sum())/ismc.sum())
+    print '\n'
+    if keep_probability is None:
+        print 'Forcing fractions to be the same'
+        keep_probability = fraction
+
+    if keep_probability > 0:
+        mask = np.invert(ismc) | \
+               (ismc & isb) | \
+               (ismc & np.invert(isb) & (np.random.rand(isMC_all.shape[0]) < keep_probability))
+    else:
+        mask = np.invert(ismc) | \
+               (ismc & np.invert(isb)) | \
+               (ismc & isb & (np.random.rand(isMC_all.shape[0]) < -1*keep_probability))
+    
+    X_all    = X_all[mask]		
+    isB_all  = isB_all[mask]
+    isMC_all = isMC_all[mask]
+    ismc = (isMC_all.ravel() > 0.5)
+    #make data and mc 50%
+    mc_frac = (float(ismc.sum())/ismc.shape[0])
+    if mc_frac > 0.5:
+        dropout = float(np.invert(ismc).sum())/float(ismc.sum())
+        mask = np.invert(ismc) | (ismc & (np.random.rand(isMC_all.shape[0]) < dropout))
+    else:
+        dropout = float(ismc.sum())/float(np.invert(ismc).sum())
+        mask = ismc | (np.invert(ismc) & (np.random.rand(isMC_all.shape[0]) < dropout))
+
+    X_all    = X_all[mask]		
+    isB_all  = isB_all[mask]
+    isMC_all = isMC_all[mask]
+    ismc = (isMC_all.ravel() > 0.5)
+    isb  = (isB_all.ravel() > 0.5)
+    print 'After masking:'
+    print 'MC fraction: %.3f' % (float(ismc.sum())/ismc.shape[0])
+    print 'Data B fraction: %.3f' % (((isb & np.invert(ismc)).sum())/float(np.invert(ismc).sum()))
+    print 'MC B fraction: %.3f' % (((isb & ismc).sum())/float(ismc.sum()))
+    print '\n'
+    input_weights = ((isb == 1) & ismc).astype(float)
+    input_weights = input_weights.reshape((input_weights.shape[0],1))
+
+    nb_mc = (isb & ismc).sum()
+    nl_mc = (ismc & np.invert(isb)).sum()
+    data_b_frac = (np.invert(ismc) & isb).sum() / float((np.invert(ismc)).sum())
+    print 'MC: #B: %d #L: %d' % (nb_mc, nl_mc)
+    exp_f = data_b_frac*nl_mc/(nb_mc*(1-data_b_frac))
+    exp_f -= 1
+    print 'Expected weight: %.3f' % exp_f
+    
+    return X_all, isB_all, isMC_all, input_weights, exp_f
